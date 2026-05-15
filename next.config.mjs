ed5b1a188b6b56b26dd501ef24a8696b6bc7e12d
trancy90 <trancy90@gmail.com>
2026-05-14 22:28:00 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    typedRoutes: true
-  }
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
